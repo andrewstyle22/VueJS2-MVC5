27ad925b3a6fe2f144670e4a0d3161bf45559af2
José Andrés Duque Andrade <andres.duque@ultebra.eu>
2019-07-08 10:45:29 +0100

    first commit

--- VueJS2-MVC5/src/components/Home.vue
+++ VueJS2-MVC5/src/components/Home.vue
@@ -1,7 +1,9 @@
 <template>
   <div class="hello">
     <h1>{{ msg }}</h1>
-<button @click="Redirect" >Go and test the banckend</button>
+    <button @click="Redirect">Go and test the banckend</button>
+    <input type="file" id="selectPDF" v-on:change="openPDF" accept="application/pdf" />
+    <span class="error">{{errorMsg}}</span>
   </div>
 </template>
 
@@ -9,14 +11,32 @@
 export default {
   data() {
     return {
-      msg: "Boilerplate: VueJS 2 + ASP.NET MVC 5"
+      msg: "Boilerplate: VueJS 2 + ASP.NET MVC 5",
+      pdf: '',
+      baseUrl: '',
+      errorMsg: '',
     };
   },
   methods: {
     Redirect() {
       this.$router.push("/APITest");
+    },
+    openPDF(e) {
+      console.log('e: ', e);
+      this.pdf = window.URL.createObjectURL(e.target.files[0])
     }
   }
 };
 </script>
+<style>
+  /* global styles */
 
+  body {
+    height: 100vh;
+  }
+
+  #app,
+  .container {
+    height: 100%;
+  }
+</style>
